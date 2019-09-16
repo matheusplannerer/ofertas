01bca833ad20e5eaad79a39f2dfa4a986149682b
Matheus <matheusplannerer@gmail.com>
2019-09-16 11:10:13 -0300

    Adicionado um espaço
    Testando gitkraken

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
+      theme: ThemeData( 
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
