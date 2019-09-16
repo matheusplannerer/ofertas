ccaf5ec6db3af694e0890a98d52ca7f5082b64f6
Matheus <matheusplannerer@gmail.com>
2019-09-16 11:12:38 -0300

    "login" - Testando gitkraken apenas

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+
+
+  ///Fiz a pagina de login ta pronta é isso
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
