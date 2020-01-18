ee88788588835d3d57a43718dbef92eba2837ba6
Ulisses <ulissrs157@gmail.com>
2020-01-18 19:44:36 -0300

    Página assinatura consertada
    Adicionei um ListView pra não bugar o botão

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\bruno_teles\Desktop\ofertas"
+export "FLUTTER_ROOT=C:\Users\uliss\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\uliss\Desktop\repositoguinho\ofertas\ofertas"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Flutter\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\uliss\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
