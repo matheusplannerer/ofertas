d33b084afb6f298adfb44778a8fbc6bcfd5de014
Bruno Arantes <brunoarantes30@yahoo.com.br>
2020-04-07 09:10:29 -0300

    Delete da imagem atualiza n° ofertas da empresa

--- lib/app/pages/oferta_details/oferta_details_controller.dart
+++ lib/app/pages/oferta_details/oferta_details_controller.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
 import 'package:ofertas/app/shared/models/oferta_model.dart';
+import 'package:ofertas/app/shared/models/perfil_empresa_model.dart';
 import 'package:ofertas/app/shared/repositories/routes/route_controller.dart';
 
 part 'oferta_details_controller.g.dart';
@@ -14,10 +15,15 @@ abstract class _OfertaDetailsControllerBase with Store {
 
   @observable
   Future deleteImage(OfertaModel oferta) async {
+    var doc = await Firestore.instance
+        .collection('empresas')
+        .document(oferta.empresaDona)
+        .get();
+    var aux = PerfilEmpresaModel.fromJson(doc.data);
     await Firestore.instance
         .collection('ofertas')
         .document(oferta.idOferta)
-        .updateData({'mostrar': false});
+        .updateData({'mostrar': false, 'ofertas': aux.ofertas - 1});
     return true;
   }
 
